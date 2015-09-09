1438771db53602a744f71da16240abfacdf64249
zlien111 <zhu303490@gmail.com>
2015-09-09 17:45:08 +0800

    create a new project, add a view controller, add a welcome label with code

--- WeChatCode/AppDelegate.m
+++ WeChatCode/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    ViewController *rootViewController = [[ViewController alloc] init];
+    [self.window setRootViewController:rootViewController];
     // Override point for customization after application launch.
     return YES;
 }
--- WeChatCode/ViewController.m
+++ WeChatCode/ViewController.m
@@ -9,16 +9,31 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UILabel * welcomeLabel;
+}
 @end
-
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self initWelcomeLabel];
+        // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)initWelcomeLabel{
+    int welcomeLabelH = 20;
+    int welcomeLabelW = 200;
+    welcomeLabel = [[UILabel alloc] initWithFrame:CGRectMake((SCREEN_WIDTH - welcomeLabelW) / 2, (SCREEN_HEIGHT - welcomeLabelH) / 2, welcomeLabelW, welcomeLabelH)];
+    welcomeLabel.textAlignment = NSTextAlignmentCenter;
+    welcomeLabel.text = @"Welcome to WeChat";
+    welcomeLabel.textColor = [UIColor blackColor];
+    welcomeLabel.font = [UIFont systemFontOfSize:16];
+    welcomeLabel.adjustsFontSizeToFitWidth = YES;
+    [self.view addSubview:welcomeLabel];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
