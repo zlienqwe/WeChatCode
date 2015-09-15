c0dc1e48fd2ef8eb0bca08e742e671eeb16e7672
Zlien <zhu303490@gmail.com>
2015-09-15 14:30:04 +0800

    modify class method

--- WeChatCode/MomentsAPI.m
+++ WeChatCode/MomentsAPI.m
@@ -6,21 +6,16 @@
 //  Copyright (c) 2015 Zlien. All rights reserved.
 //
 
-#import "UrlHelper.h"
+#import "MomentsAPI.h"
 #import "AFNetworking.h"
 
+@implementation MomentsAPI
 
-@implementation UrlHelper
-
-- (void)requestHeaderViewInfo:(void (^)(NSDictionary *))success failure:(void (^)())failure {
++ (void)requestHeaderViewInfo:(void (^)(NSDictionary *))success failure:(void (^)())failure {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-     
     [manager GET:UserInfoUrl parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject){
-
         if ([responseObject isKindOfClass:[NSDictionary class]]) {
-            
-            NSDictionary *dic = responseObject;
-            success(dic);
+            success(responseObject);
         }
     }failure:^(AFHTTPRequestOperation *operation, NSError *error){
         NSLog(@"error %@",error);
@@ -28,12 +23,9 @@
     }];
 }
     
--(void)requestFeeds:(void (^)(NSArray *))success failure:(void (^)())failure{
-    
++ (void)requestFeeds:(void (^)())success failure:(void (^)())failure {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    
     [manager GET:ContentUrl parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject){
-        
         if ([responseObject isKindOfClass:[NSArray class]]) {
             success(responseObject);
         }
